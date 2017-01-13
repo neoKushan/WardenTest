3524088e3676b5b936ef13e5278766ded0b68acc
Stephen Donaghy <t3h.kitteh@gmail.com>
2017-01-13 22:36:26 +0000

    Initial Service based off of example

---
+++ src/Warden.WindowsService/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using Topshelf;
+
+namespace Warden.WindowsService
+{
+    public class Program
+    {
+        private const string ServiceName = "Warden";
+
+        public static void Main(string[] args)
+        {
+            args = args.Where(a => a != ServiceName).ToArray();
+            HostFactory.Run(x =>
+            {
+                x.ApplyCommandLine(string.Join(" ", args));
+                x.Service<WardenService>(service =>
+                {
+                    service.ConstructUsing(name => new WardenService());
+                    service.WhenStarted(async warden => await warden.StartAsync());
+                    service.WhenPaused(async warden => await warden.PauseAsync());
+                    service.WhenContinued(async warden => await warden.StartAsync());
+                    service.WhenStopped(async warden => await warden.StopAsync());
+                });
+                x.RunAsLocalSystem();
+                x.SetDescription(ServiceName);
+                x.SetDisplayName(ServiceName);
+                x.SetServiceName(ServiceName);
+            });
+        }
+    }
+}
